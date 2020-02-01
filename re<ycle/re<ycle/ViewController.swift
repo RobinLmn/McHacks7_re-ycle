29cdb506d7239343e557e9e7515a6fcc15c48d00
Robin Leman <robin.leman@mail.mcgill.ca>
2020-02-01 14:57:30 -0500

    Recreated XCode Project
    First initialization was wrong

---
+++ re<ycle/re<ycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  re<ycle
+//
+//  Created by Robin Leman on 01/02/2020.
+//  Copyright © 2020 Lorne & Leman Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
